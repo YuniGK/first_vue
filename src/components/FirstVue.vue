e6bd078df4c1192db0dd8578530252b4b20ed282
YuniGK <1106qwe@hanmail.net>
2022-06-26 16:50:29 +0900

    button add

---
+++ src/components/FirstVue.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+
+    <h2>vue</h2>
+    <ul>
+      <li v-for="(n, i) in menus" :key="i"><a href="#">{{i}}___{{n}}</a></li>
+    </ul>
+
+    <p v-for="(v, i) in products" :key="i">
+      {{v}}
+
+      <button :class="[i+'_btn']">click</button>
+      <span>count : <span :class="[i+'_count']"></span></span>
+    </p>
+
+  </div>
+</template>
+
+<script>
+let val = [];
+for (let i = 0; i < 10; i++) {
+  val[i] = i+1;
+}
+
+export default {
+  name: 'FirstVue',
+
+  data(){
+    //데이터 보관함
+    return {
+      menus : ['Home', 'Shop', 'About'],
+      //products : [1,2,3,4,5]
+      products : val
+    }
+  },
+}
+</script>
+
+<style scoped>
+  ul {display: flex;}
+  li {flex-direction: row; flex-wrap: wrap; justify-self: center;
+  justify-content: center; align-items: center; width: 33.3%; text-align: center;} 
+</style>
